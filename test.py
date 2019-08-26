0eb2ee1325daeaaf92e3a84577b705a72856a591
Santhosh Balasa <santhosh.kbr@gmail.com>
2019-08-26 08:19:41 +0530

    Created test to measure the caching speeds
     To measure the caching speeds of lru based and memoized based caching mechanisms

---
+++ test.py
@@ -0,0 +1,49 @@
+from .caching import lru_cahce, memoize
+from timeit import timeit
+
+
+@lru_cache(None):
+def fib_lru_cache(n):
+"""
+Generate fibonacci series using lru caching mechanism
+"""
+if n == 0:
+    return [0]
+elif n == 1:
+    return [0, 1]
+else:
+    fib_series = [0, 1]
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a+b
+        fib_series.append(b)
+    return fib_series
+
+@memoize
+def fib_memoized_cache(n):
+"""
+Generate fibonacci series using memoized caching
+"""
+if n == 0:
+    return [0]
+elif n == 1:
+    return [0, 1]
+else:
+    fib_series = [0, 1]
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a+b
+        fib_series.append(b)
+    return fib_series
+ 
+
+def test_lru_caching():
+    print(timeit("fib_lru_cache(100)", setup = "from __main__ import fib_lru_cache"))
+
+def test_memoized_caching():
+    print(timeit("fib_memoized_cache(100)", setup = "from __main__ import fib_memoized_cache"))
+
+
+if __name__ == "__main__":
+    test_lru_caching()
+    test_memoized_caching()
