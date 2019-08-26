28dc63672beec65f00a7002e4a6ee4b0b7955be8
Santhosh Balasa <santhosh.kbr@gmail.com>
2019-08-26 08:00:30 +0530

    Creating lru_cache and memoized cache

---
+++ caching.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+
+def memoize(func):
+    cache = {}
+    def memoized_func(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+    return memoized_func
